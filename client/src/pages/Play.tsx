a2290891620c0c8d5007573f2818a170e70150cf
deewau <skvortsov3401@gmail.com>
2026-04-29 01:45:27 +0300

    dsadasdasd'

--- client/src/pages/Play.tsx
+++ client/src/pages/Play.tsx
@@ -130,7 +130,7 @@ const PROBABILITIES_VIP: Record<string, number> = {
   'lolpop': 0.70,
   'snakebox': 0.81,
   // VIRT подарки (разные ID для вероятностей, но один файл virt.json)
-  'virt240': 2.27,
+  'virt': 2.27,
   // 'virt490': 1.18,
 };
 
