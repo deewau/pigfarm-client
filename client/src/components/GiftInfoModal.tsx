da2a02e43a4f019fde58bd7e8778d4b5a1e82111
deewau <skvortsov3401@gmail.com>
2026-04-29 01:15:22 +0300

    dsadasdasd

--- client/src/components/GiftInfoModal.tsx
+++ client/src/components/GiftInfoModal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import lottie from 'lottie-web';
+import { GiftImage } from './GiftAnimation';
 import './GiftInfoModal.css';
 
 interface GiftInfoModalProps {
